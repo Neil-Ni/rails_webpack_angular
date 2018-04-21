001cdd0c89b56096b266c69960858be596642007
Neil Ni <tzuyangni@gmail.com>
2018-04-21 17:22:17 -0400

    add loaders

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,24 @@ const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.append('typescript', typescript)
+
+environment.loaders.append('html', {
+  test: /\.html$/,
+  exclude: /node_modules/,
+  loaders: ['html-loader']
+})
+
+environment.loaders.prepend('sass', {
+  test: /\.(css|scss|sass)$/,
+  use: [{
+    loader: "to-string-loader"
+  }, {
+    loader: "css-loader"
+  }, {
+    loader: "resolve-url-loader"
+  }, {
+    loader: "sass-loader"
+  }]
+})
+
 module.exports = environment
